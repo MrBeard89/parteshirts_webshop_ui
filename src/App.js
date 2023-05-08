66d6fb363f0728f169669b10f18a0481fc4023b1
Norbert Kovács <k.n.89@outlook.hu>
2023-05-08 10:38:12 +0200

    Added Logo.jsx,displayed logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.scss'
+import { Logo } from './components/Logo/Logo'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Logo />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import Sitelogo from '../../assets/logo/parteeshirts.jpg'
+
+export const Logo = () => {
+  return (
+    <div className='logo-container'>
+      <img src={Sitelogo} alt='Logo of Parteeshirts' />
+    </div>
+  )
+}
