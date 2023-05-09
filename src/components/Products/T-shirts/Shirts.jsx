3a98aa940b95385e8bf678adeefe2de5675a5101
Norbert Kovács <k.n.89@outlook.hu>
2023-05-09 19:02:51 +0200

    Home-Products design finished(notresponsive)

--- src/components/Products/T-shirts/Shirts.jsx
+++ src/components/Products/T-shirts/Shirts.jsx
@@ -1,9 +1,5 @@
 import '../../../styles/Products/T-shirts/Shirts.scss'
 
 export const Shirts = () => {
-  return (
-    <div className='shirts-wrapper'>
-      <div className='shirts-container'>hello</div>
-    </div>
-  )
+  return
 }
