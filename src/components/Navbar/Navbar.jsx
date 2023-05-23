908d5774671180e24f57356e1a46752e3863f5c1
Norbert Kovács <k.n.89@outlook.hu>
2023-05-23 20:20:50 +0200

    Navbar mobile menu + cart menu design finished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,17 +2,27 @@ import '../../styles/Navbar/Navbar.scss'
 import { Link } from 'react-router-dom'
 
 import { Logo } from '../../components/Logo/Logo'
+import MobileVector from '../../assets/logo/emoji-vector.png'
 
-import { GiBurningSkull } from 'react-icons/gi'
+import { GiBurningSkull, GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineClose } from 'react-icons/ai'
+import { HiOutlineShoppingBag } from 'react-icons/hi'
+import { useState } from 'react'
 
 export const Navbar = () => {
+  //Usestate for navbar open,close
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  //Usestate for cart open,close
+  const [cartOpen, setCartOpen] = useState(false)
+
   var prevScrollpos = window.pageYOffset
   window.onscroll = function () {
     var currentScrollPos = window.pageYOffset
     if (prevScrollpos > currentScrollPos) {
       document.querySelector('.navbar-wrapper').style.top = '0'
     } else {
-      document.querySelector('.navbar-wrapper').style.top = '-180px'
+      document.querySelector('.navbar-wrapper').style.top = '-80px'
     }
     prevScrollpos = currentScrollPos
   }
@@ -21,55 +31,77 @@ export const Navbar = () => {
     <>
       <div className='navbar-wrapper'>
         <div className='navbar-container'>
+          {/* Mobile icon container - menu*/}
+          <span className='mobile-icon-container' onClick={() => setNavbarOpen((prev) => !prev)}>
+            {navbarOpen ? (
+              <AiOutlineClose className='menu-close' />
+            ) : (
+              <GiHamburgerMenu className='hamburger' />
+            )}
+          </span>
+
           <div className='navbar-logo'>
             <Link to='/home'>
               <Logo />
             </Link>
           </div>
 
-          <ul className='navlinks-container'>
-            <Link to='/home'>
-              <GiBurningSkull className='skull' />
-              <li className='navlink'>Home</li>
-              <GiBurningSkull className='skull-back' />
-            </Link>
+          {/* Mobile icon container - cart*/}
+          <span className='cart-icon-container' onClick={() => setCartOpen((prev) => !prev)}>
+            {cartOpen ? (
+              <AiOutlineClose className='cart-close' />
+            ) : (
+              <HiOutlineShoppingBag className='cart' />
+            )}
+          </span>
 
-            <Link to='/'>
-              <GiBurningSkull className='skull' />
-              <li className='navlink'>Search</li>
-              <GiBurningSkull className='skull-back' />
-            </Link>
+          <div className={`${navbarOpen ? 'navbar-mobile' : 'navlinks-container'}`}>
+            <ul className='navlinks' onClick={() => setNavbarOpen(false)}>
+              <Link to='/home'>
+                <li className='navlink'>Home</li>
+                <GiBurningSkull className='skull-back' />
+              </Link>
 
-            <Link to='/'>
-              <GiBurningSkull className='skull' />
-              <li className='navlink'>T-shirts</li>
-              <GiBurningSkull className='skull-back' />
-            </Link>
+              <Link to='/'>
+                <li className='navlink'>Search</li>
+                <GiBurningSkull className='skull-back' />
+              </Link>
 
-            <Link to='/'>
-              <GiBurningSkull className='skull' />
-              <li className='navlink'>Hoodies</li>
-              <GiBurningSkull className='skull-back' />
-            </Link>
+              <Link to='/'>
+                <li className='navlink'>T-shirts</li>
+                <GiBurningSkull className='skull-back' />
+              </Link>
 
-            <Link to='/'>
-              <GiBurningSkull className='skull' />
-              <li className='navlink'>FAQ's</li>
-              <GiBurningSkull className='skull-back' />
-            </Link>
+              <Link to='/'>
+                <li className='navlink'>Hoodies</li>
+                <GiBurningSkull className='skull-back' />
+              </Link>
 
-            <Link to='/'>
-              <GiBurningSkull className='skull' />
-              <li className='navlink'>Reviews</li>
-              <GiBurningSkull className='skull-back' />
-            </Link>
+              <Link to='/'>
+                <li className='navlink'>FAQ's</li>
+                <GiBurningSkull className='skull-back' />
+              </Link>
 
-            <Link to='/'>
-              <GiBurningSkull className='skull' />
-              <li className='navlink'>Cart</li>
-              <GiBurningSkull className='skull-back' />
-            </Link>
-          </ul>
+              <Link to='/'>
+                <li className='navlink'>Reviews</li>
+                <GiBurningSkull className='skull-back' />
+              </Link>
+            </ul>
+
+            <div className='img-container'>
+              <img src={MobileVector} alt='Emoji-vector-img' />
+            </div>
+          </div>
+
+          <div className={`${cartOpen ? 'cart-mobile' : 'cartlinks-container'}`}>
+            <div className='cart-wrapper'>
+              <h3 className='title'>Your shopping cart</h3>
+              <p className='cart-message'>Your shopping cart is empty</p>
+              <button className='goshoppingBtn' onClick={() => setCartOpen(false)}>
+                Go Shopping
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
