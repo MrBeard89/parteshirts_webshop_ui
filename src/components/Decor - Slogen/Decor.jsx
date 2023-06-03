77d6c840d4db01c3e3fee2bb32e002b522b0996d
Norbert Kovács <k.n.89@outlook.hu>
2023-06-03 19:36:26 +0200

    Created Decor animation component

---
+++ src/components/Decor - Slogen/Decor.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../styles/Decor - Slogen/Decor.scss'
+
+const Decor = () => {
+  return (
+    <div
+      className='decor-container'
+      style={{ height: '3rem', width: '100%', backgroundColor: 'black' }}
+    >
+      <p className='decor-text'>Parteeshirts | Feel the Vibe</p>
+    </div>
+  )
+}
+
+export default Decor
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,16 +8,19 @@ import { FAQ } from '../../components/FAQ/FAQ'
 import { Contact } from '../../components/Contact/Contact'
 import { Footer } from '../../components/Footer/Footer'
 import { Products } from '../../components/Products/Products'
+import Decor from '../../components/Decor - Slogen/Decor'
 
 export const Home = () => {
   return (
     <div className='home-wrapper'>
+      <Decor />
       <Carousel />
       <About />
       <Products />
       <Bestsellers />
       <FAQ />
       <Contact />
+      <Decor />
       <Footer />
     </div>
   )
