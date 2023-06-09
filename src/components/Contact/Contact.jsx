774e0a6581d434a31c37aa6e8d4be0d09c752591
Norbert Kovács <k.n.89@outlook.hu>
2023-06-09 11:09:52 +0200

    Contact form finished,responsive,frontend validat

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,102 @@
 import '../../styles/componentstyles/Home/Contact/Contact.scss'
 
-import { RxDoubleArrowDown } from 'react-icons/rx'
-
 export const Contact = () => {
   return (
-    <div className='contact-wrapper'>
-      <div className='contact-container'>
-        <div className='contact-title-bar'>
-          <h1 className='title-bar'>Contact us</h1>
-        </div>
-        <div className='text-container'>
-          <p className='contact-text'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque molestias quo nesciunt!
-            Perferendis tenetur odit ea unde magni, consequuntur iusto inventore accusamus laborum
-            molestias culpa. Repellat aspernatur obcaecati quisquam consectetur harum
-            necessitatibus, odit suscipit perferendis voluptatum amet possimus pariatur itaque
-            temporibus laudantium deserunt porro doloribus voluptate perspiciatis nemo animi
-            accusantium!
-          </p>
-          <button>Send Email</button>
-          <RxDoubleArrowDown className='down-arrow' />
-        </div>
+    <div className='contact-wrapper' id='contact'>
+      {/*  Main container */}
 
-        <button className='reviews'>View Reviews</button>
+      <div className='contact-container'>
+        {/* Main title */}
+
+        <h1 className='contact-title'>Contact us</h1>
+
+        {/*Contact Outro text */}
+
+        <p className='contact-text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, perspiciatis! Tempora
+          commodi aut veritatis fugiat omnis incidunt aliquam, eos dolores.{' '}
+        </p>
+
+        {/* Main form container */}
+
+        <div className='contact-form-container'>
+          {/* Actual form */}
+          {/* Validation is done by pure html :valid,:invalid,:required,and :focus pseudo classes via css/html*/}
+
+          <form className='contact-form' action='#' method='#'>
+            {/* Each div is a form field */}
+
+            <div className='contact-form-field'>
+              {/* Label for Name */}
+              <label className='contact-form-label' htmlFor='name'>
+                Name
+              </label>
+
+              {/* Input - Name */}
+
+              <input
+                className='contact-form-input'
+                type='text'
+                name='name'
+                placeholder='Please write your name'
+                pattern='^[a-zA-ZÀ-ÖØ-öø-ÿ ]+$'
+                required
+              />
+              <span className='en'></span>
+            </div>
+
+            <div className='contact-form-field'>
+              {/* Label for Email */}
+
+              <label className='contact-form-label' htmlFor='email'>
+                Email
+              </label>
+
+              {/* Input - Email */}
+
+              <input
+                className='contact-form-email'
+                type='email'
+                name='email'
+                placeholder='Please write an Email'
+                required
+                minLength='1'
+              />
+              <span className='en'></span>
+            </div>
+
+            <div className='contact-form-field'>
+              {/* Label for Message */}
+
+              <label className='contact-form-label' htmlFor='message'>
+                Message
+              </label>
+
+              {/* Textarea */}
+
+              <textarea
+                className='contact-form-textarea'
+                cols='30'
+                rows='10'
+                type='text'
+                name='message'
+                placeholder='Please write a Message'
+                required
+                autoComplete='none'
+                minLength='10'
+              />
+              <span className='en'></span>
+            </div>
+
+            {/* Submit button - no action,no method , just snaps at top of the page */}
+
+            <div className='submit-btn-container'>
+              <button className='btn-submit' type='submit'>
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
