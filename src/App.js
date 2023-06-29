a34ee32790b26ad5f55cdc08174bb7e1c935f414
Norbert Kovács <k.n.89@outlook.hu>
2023-06-29 17:57:51 +0200

    Set basename for Router

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className='App'>
       <ShopContextProvider>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
